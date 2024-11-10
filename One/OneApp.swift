131688cc242543a5c4d5ab76f312deed501eebf0
treylee <trieveon.lee@gmail.com>
2024-11-10 13:54:52 -0500

    Initial Commit

---
+++ One/OneApp.swift
@@ -0,0 +1,17 @@
+//
+//  OneApp.swift
+//  One
+//
+//  Created by Trieveon Cooper on 11/10/24.
+//
+
+import SwiftUI
+
+@main
+struct OneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ OneTests/OneTests.swift
@@ -0,0 +1,17 @@
+//
+//  OneTests.swift
+//  OneTests
+//
+//  Created by Trieveon Cooper on 11/10/24.
+//
+
+import Testing
+@testable import One
+
+struct OneTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
